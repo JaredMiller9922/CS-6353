7b4da9b7602fd586722a1d0fd2118867f75361aa
JaredMiller9922 <jaredmschool9922@gmail.com>
2025-09-26 18:11:39 -0600

    tries again fails again

--- Homeworks/assignment2/cs6353/classifiers/linear_svm.py
+++ Homeworks/assignment2/cs6353/classifiers/linear_svm.py
@@ -41,7 +41,7 @@ def svm_loss_naive(W, X, y, reg):
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-  dW /= num_train # We must also average the dW because if the loss is averaged that means we also have a 1/N in the gradient
+  dW /= num_train # We must also average the dW because if the loss is averaged that means we also have a 1/N in the gradient as well
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
@@ -67,9 +67,36 @@ def svm_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as svm_loss_naive.
   """
-  loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
+  loss = 0.0
+  scores = X @ W 
 
+  # Find a vectorized way to get the correct class for each row (example)
+  correct_scores = scores[np.arange(scores.shape[0]), y].reshape(-1, 1)
+  margins = np.maximum(0, scores - correct_scores + 1)
+
+  # We don't want the correct classes to contribute to the margins
+  margins[np.arange(margins.shape[0]), y] = 0
+
+  # Update true classes gradients
+  violation_count = np.count_nonzero(margins, axis = 1)
+  dW[:, y] -= (violation_count.reshape(-1,1) * X).T
+
+  # Update non true classes gradients
+  incorrect_classes = margins > 0
+  print("shape of incorect classes")
+  print(incorrect_classes.shape)
+  print("shape of incorect classes")
+  print(dW.shape)
+  dW[incorrect_classes] += X 
+ 
+
+  loss = np.sum(margins)
+  loss /= X.shape[0]
+  loss += reg * np.sum(W * W)
+  
+  dW /= X.shape[0]
+  dW += reg * 2 * np.dot(W,W)
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
