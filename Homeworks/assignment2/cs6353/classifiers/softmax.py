34447633b854d9671b54d1eadee7f577211b4de8
jared <jaredmschool9922@gmail.com>
2025-09-28 14:06:06 -0600

    Somehow became a biggest brain boi!

--- Homeworks/assignment2/cs6353/classifiers/softmax.py
+++ Homeworks/assignment2/cs6353/classifiers/softmax.py
@@ -29,10 +29,27 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  # For each training example in X
+  for i in range(num_train):
+    scores = X[i].dot(W)
+    correct_class_score = scores[y[i]]
+    denominator = np.sum(np.exp(scores))
+    loss += -1 * np.log(np.exp(correct_class_score) / denominator)
+
+    for j in range(num_classes):
+      s_j = np.exp(scores[j]) / denominator
+      if j == y[i]:
+        dW[:, y[i]] += X[i]*(s_j - 1) + np.dot(W[j],W[j]) * 2 * reg
+      else:
+        dW[:, j] += s_j * X[i] + np.dot(W[j],W[j]) * 2 * reg
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
+  loss /= num_train
+  dW /= num_train
 
   return loss, dW
 
@@ -53,7 +70,23 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  correct_class_scores = scores[np.arange(scores.shape[0]), y] 
+  denominators = np.sum(np.exp(scores), axis = 1)
+  loss_vec = -1 * np.log(np.exp(correct_class_scores) / denominators)
+  loss = np.sum(loss_vec)
+  loss /= X.shape[0]
+
+  s_js = np.exp(scores) / denominators.reshape(-1, 1)
+
+  # Noticing that in the gradient the only difference between the updates is that when 
+  # j == y[i] we simply subtract 1 from s_j
+  s_js[np.arange(s_js.shape[0]), y] -= 1
+  dW = X.T @ s_js
+  
+  dW /= X.shape[0]
+  dW += 2 * W * reg
+  
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
